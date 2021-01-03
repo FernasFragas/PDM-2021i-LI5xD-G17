294e013e3b266dabb965aa3ab2e3165cd5d9d55e
cotinhalol <vizinhandre@gmail.com>
2021-01-03 17:34:25 +0000

    menu update

---
+++ app/src/main/java/pt/isel/pdm/drag/list_games_activity/ListGamesActivity.kt
@@ -0,0 +1,159 @@
+package pt.isel.pdm.drag.list_games_activity
+
+import android.app.Activity
+import android.content.Intent
+import android.os.Bundle
+import android.os.Parcelable
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.activity.viewModels
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import pt.isel.pdm.drag.R
+import pt.isel.pdm.drag.databinding.ActivityListGamesLayoutBinding
+import pt.isel.pdm.drag.list_games_activity.view.ChallengesListAdapter
+
+
+private const val CREATE_CODE = 10001
+
+class ListGamesActivity : AppCompatActivity(){
+
+    private fun updateChallengesList() {
+        binding.refreshLayout.isRefreshing = true
+        //viewModel.fetchChallenges()
+    }
+
+    /**
+     * Called whenever a list element is selected. The player that accepts the challenge is the
+     * first to make a move.
+     *
+     * @param challenge the selected challenge
+     */
+    private fun challengeSelected(challenge: ChallengeInfo) {
+        /*
+        TODO falta fazer o viewModel.tryAcceptChallenge (acesso ao repo)
+        AlertDialog.Builder(this)
+            .setTitle(getString(R.string.accept_challenge_dialog_title, challenge.challengerName))
+            .setPositiveButton(R.string.accept_challenge_dialog_ok) { _, _ -> viewModel.tryAcceptChallenge(challenge) }
+            .setNegativeButton(R.string.accept_challenge_dialog_cancel, null)
+            .create()
+            .show()
+
+         */
+    }
+
+
+    private val binding: ActivityListGamesLayoutBinding by lazy {
+        ActivityListGamesLayoutBinding.inflate(layoutInflater)
+    }
+
+    private val viewModel: ListGamesViewModel by viewModels()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+
+        binding.challengesList.setHasFixedSize(true)
+        binding.challengesList.layoutManager = LinearLayoutManager(this)
+
+        // Get view model instance and add its contents to the recycler view
+        viewModel.challenges.observe(this) {
+            binding.challengesList.adapter = ChallengesListAdapter(it, ::challengeSelected)
+            binding.refreshLayout.isRefreshing = false
+        }
+
+        // Setup ui event handlers
+        binding.refreshLayout.setOnRefreshListener {
+            updateChallengesList()
+        }
+
+        binding.createChallengeButton.setOnClickListener {
+            /*
+            //TODO lançar o challenge
+            startActivityForResult(Intent(this, CreateChallengeActivity::class.java),
+                    CREATE_CODE
+            )
+
+             */
+        }
+
+        viewModel.enrolmentResult.observe(this) {
+
+            if (it.state == State.COMPLETE) {
+                if (it.result != null) {
+                    /*
+                    TODO lançar a atividade do jogo se possivel
+                    startActivity(Intent(this, GameActivity::class.java).apply {
+                        putExtra(ACCEPTED_CHALLENGE_EXTRA, it.result)
+                        putExtra(LOCAL_PLAYER_EXTRA, Player.P1 as Parcelable)
+                        putExtra(TURN_PLAYER_EXTRA, Player.P1 as Parcelable)
+                    })
+
+                     */
+                } else {
+                    /*
+                    TODO ERRO AO LANÇAR ATIVIDADE DO JOGO
+                    Toast.makeText(this, R.string.error_accepting_challenge, Toast.LENGTH_LONG).show()
+
+                     */
+                }
+                viewModel.resetEnrolmentResult()
+                binding.challengesList.isEnabled = true
+            } else {
+                binding.challengesList.isEnabled = false
+            }
+        }
+    }
+
+
+
+    /**
+     * Callback method that handles menu creation
+     */
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_challenges_list, menu)
+        return true
+    }
+
+    /**
+     * Callback method that handles menu item selection
+     */
+    override fun onOptionsItemSelected(item: MenuItem): Boolean = when (item.itemId) {
+        R.id.challenges_list_update -> {
+            updateChallengesList()
+            true
+        }
+        else -> super.onOptionsItemSelected(item)
+    }
+
+    /**
+     * Callback method that handles the result obtained from activities launched to collect user
+     * input.
+     */
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        when (requestCode) {
+            CREATE_CODE -> if (resultCode == Activity.RESULT_OK) {
+                updateChallengesList()
+                /*
+                //TODO NÃO TENHO A CERTEZA
+                val createdChallenge = data?.getParcelableExtra<ChallengeInfo>(RESULT_EXTRA)
+                startActivity(Intent(this, GameActivity::class.java).apply {
+                    putExtra(ACCEPTED_CHALLENGE_EXTRA, createdChallenge)
+                    putExtra(LOCAL_PLAYER_EXTRA, Player.P2 as Parcelable)
+                    putExtra(TURN_PLAYER_EXTRA, Player.P1 as Parcelable)
+                })
+
+                 */
+            }
+            else -> super.onActivityResult(requestCode, resultCode, data)
+        }
+    }
+
+
+    override fun onStart() {
+        super.onStart()
+        updateChallengesList()
+    }
+}
--- app/src/main/java/pt/isel/pdm/drag/onGoingGamesActivity/OnGoingGamesActivity.kt
+++
@@ -1,7 +0,0 @@
-package pt.isel.pdm.drag.onGoingGamesActivity
-
-import androidx.appcompat.app.AppCompatActivity
-
-class OnGoingGamesActivity : AppCompatActivity(){
-
-}
